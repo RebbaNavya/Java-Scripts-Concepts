cd8a2f23d02c09ddb8827271374838fba6ac9ab3
Rebba Navya <rebbanavya122gmail.com>
2020-04-22 18:43:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//In Javascript Everything is object (methods,fucntions,arrays ,string are all objects by defult in javascript)
+//let,const are block scoping its accessed within the function scope only
+// var lexical scoping where we can access globally
+
+
+const value={
+    name:"navya"
+}
+
+console.log(name);//const are block scoping its accessed within the function scope only
