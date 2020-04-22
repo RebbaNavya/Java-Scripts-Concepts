c9d838dc9dd1e9eaa6a70e237b323183468bd9f3
Rebba Navya <rebbanavya12@gmail.com>
2020-04-22 19:11:18 +0530

    commit three variables

--- index.js
+++ index.js
@@ -3,8 +3,22 @@
 // var lexical scoping where we can access globally
 
 
+var name={
+    bike:"hero",
+    
+}
+
+
 const value={
     name:"navya"
 }
 
-console.log(name);//const are block scoping its accessed within the function scope only
+//console.log(name);//const are block scoping its accessed within the function scope only
+
+let number={
+    num:"12"
+}
+
+//console.log(value);//let are block scoping its accessed within the function scope only
+
+console.log(name.bike);
